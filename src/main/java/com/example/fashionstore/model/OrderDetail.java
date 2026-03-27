cfc0141679fc24434e45f0c243fe0d208a06fa21
Trần Khánh Vinh <128805837+JauntyMonkey144@users.noreply.github.com>
2026-03-27 11:25:30 +0700

    Add files via upload

---
+++ src/main/java/com/example/fashionstore/model/OrderDetail.java
@@ -0,0 +1,29 @@
+package com.example.fashionstore.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "order_details")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer quantity;
+    private Double price; // Giá tại thời điểm mua
+    private Double subTotal;
+}
